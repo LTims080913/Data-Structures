cef55f4db1ec8922f7d3faa59bc87847e08d8dc5
Latosha Tims <ltims080913@hotmail.com>
2020-06-11 11:04:45 -0700

    finished MVP for day 2

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -48,12 +48,16 @@ class DoublyLinkedList:
     as the new head of the list. Don't forget to handle 
     the old head node's previous pointer accordingly."""
     def add_to_head(self, value):
-        new_node = ListNode(value, None, self.head)
-        if self.head is not None:
-            self.head.prev = new_node
-        else:
+        new_node = ListNode(value, None, None)
+        if not self.head and not self.tail:
+            self.head = new_node
             self.tail = new_node
-        self.head = new_node
+            self.length += 1
+        else:
+            new_node.next = self.head
+            self.head.prev = new_node
+            self.head = new_node
+            self.length += 1
             
 
 
@@ -61,47 +65,85 @@ class DoublyLinkedList:
     current head's next node the new head of the List.
     Returns the value of the removed Node."""
     def remove_from_head(self):
-        removed = self.head
-        self.head = self.head.next
-        if self.head is not None:
-            self.head.prev = None
-        else:
-            self.tail = None
-        return removed.value
+        if not self.head:
+            return None
+        removed = self.head.value
+        self.delete(self.head)
+        return removed
+        
 
     """Wraps the given value in a ListNode and inserts it 
     as the new tail of the list. Don't forget to handle 
     the old tail node's next pointer accordingly."""
     def add_to_tail(self, value):
-        new_num = ListNode(value, None, self.tail)
-        if self.tail is not None:
-            self.tail.next = new_num
-        if self.head is None:
-            self.head = new_num
-        self.tail = new_num
+        new_node = ListNode(value, None, None)
+        if not self.head and not self.tail:
+            self.head = new_node
+            self.tail = new_node
+            self.length += 1
+        else:
+            new_node.prev = self.tail
+            self.tail.next = new_node
+            self.tail = new_node
+            self.length += 1
+            
+    
 
 
     """Removes the List's current tail node, making the 
     current tail's previous node the new tail of the List.
     Returns the value of the removed Node."""
     def remove_from_tail(self):
-        
+        if self.tail == None:
+            return None
+        removed = self.tail.value
+        self.delete(self.tail)
+        return removed
+
+
 
     """Removes the input node from its current spot in the 
     List and inserts it as the new head node of the List."""
     def move_to_front(self, node):
-        pass
+        if node is self.head:
+            return 
+        self.delete(node)
+        #if this node is the tail this function would not work because we aren't updating the tail
+        self.add_to_head(node.value)
+        return node.value
+
 
     """Removes the input node from its current spot in the 
     List and inserts it as the new tail node of the List."""
     def move_to_end(self, node):
-        pass
+        if node is self.tail:
+            return None
+        self.delete(node)
+        self.add_to_tail(node.value)
+        return node.value
 
     """Removes a node from the list and handles cases where
     the node was the head or the tail"""
     def delete(self, node):
-        pass
+        self.length -= 1
+        if self.head is self.tail:
+            self.head = None
+            self.tail = None
+        elif node is self.head:
+            self.head = node.next 
+            node.delete()
+        elif node is self.tail:
+            self.tail = node.prev
+            node.delete()
+        else:
+            node.delete()
         
     """Returns the highest value currently in the list"""
     def get_max(self):
-        pass
+        highest_val = self.head.value
+        current = self.head
+        while current is not None:
+            if current.value > highest_val:
+                highest_val = current.value
+            current = current.next
+        return highest_val
